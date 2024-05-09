5ebcd47c038388583787cb097462333b18c0c459
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2024-05-09 21:21:32 +0700

    finished tamplating

---
+++ public/js/data.js
@@ -0,0 +1,175 @@
+const trendingMusic = [
+  {
+    title: "Blinding Lights",
+    author: "The Weeknd",
+    cover: "https://example.com/blinding-lights-cover.jpg",
+    link: "https://example.com/blinding-lights-song",
+  },
+  {
+    title: "Levitating",
+    author: "Dua Lipa ft. DaBaby",
+    cover: "https://example.com/levitating-cover.jpg",
+    link: "https://example.com/levitating-song",
+  },
+  {
+    title: "Save Your Tears",
+    author: "The Weeknd",
+    cover: "https://example.com/save-your-tears-cover.jpg",
+    link: "https://example.com/save-your-tears-song",
+  },
+  {
+    title: "Montero (Call Me By Your Name)",
+    author: "Lil Nas X",
+    cover: "https://example.com/montero-cover.jpg",
+    link: "https://example.com/montero-song",
+  },
+  {
+    title: "Peaches",
+    author: "Justin Bieber ft. Daniel Caesar & Giveon",
+    cover: "https://example.com/peaches-cover.jpg",
+    link: "https://example.com/peaches-song",
+  },
+];
+
+const packagePlan = [
+  {
+    type: "Personal",
+    price: 1500,
+    benefit: [
+      "1 Akun Premium",
+      "Akses Pembatalan Kapan saja",
+      "Berlangganan atau sekali bayar",
+      "Dengarkan musik secara offline",
+    ],
+  },
+  {
+    type: "Couple",
+    price: 3000,
+    benefit: [
+      "1 Akun Premium",
+      "Akses Pembatalan Kapan saja",
+      "Berlangganan atau sekali bayar",
+      "Dengarkan musik secara offline",
+    ],
+  },
+  {
+    type: "Family",
+    price: 6500,
+    benefit: [
+      "1 Akun Premium",
+      "Akses Pembatalan Kapan saja",
+      "Berlangganan atau sekali bayar",
+      "Dengarkan musik secara offline",
+    ],
+  },
+];
+
+const blog = [
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+  {
+    thumnail: "link to img",
+    title: "title",
+    previewText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quam ad mollitia doloremque itaque quis aut! Eveniet nostrum ducimus rem?",
+    paragraf: [
+      "paragraf 1",
+      "paragraf 2",
+      "paragraf 3",
+      "paragraf 4",
+      "paragraf 5",
+      "paragraf 6",
+    ],
+    toDetail: "link to detail",
+  },
+];
+
+export default { trendingMusic, packagePlan, blog };
